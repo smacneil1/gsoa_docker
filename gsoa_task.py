71d7ad484c66baa6467ca02d09e1a6741591da92
smacneil1 <smacneil88@gmail.com>
2017-03-16 23:47:23 -0600

    updated docker file and added error email

--- gsoa_task.py
+++ gsoa_task.py
@@ -8,6 +8,7 @@ from email.MIMEBase import MIMEBase
 import smtplib
 
 gsoa = importr('GSOA')
+rmarkdown = importr('rmarkdown')
 
 conn = Redis(host="redis")
 tiger = tasktiger.TaskTiger(connection=conn)
@@ -20,23 +21,26 @@ ACCEPTED_FIELDS = ['outFilePath', 'classificationAlgorithm', 'numCrossValidation
 @tiger.task()
 def call_gsoa(request):
     print("request: {}".format(request))
-    gsoa = importr('GSOA')
-    conn = Redis(host="redis")
-    tiger = tasktiger.TaskTiger(connection=conn)
-    args = request.copy()
-    for field in NECESSARY_FIELDS:
-        args.pop(field)
-    if len(str(request.get('dataFilePath'))) < 2:
-        return "no data"
-    outFilePath = "/data/{}.txt".format(request.get('email', 'results_txt').replace('.com', ''))
-    print("email: {}".format(request.get('email', 'results_txt')))       
-    result =  gsoa.GSOA_ProcessFiles(dataFilePath=request.get('dataFilePath', ''),
-                                     classFilePath=request.get('classFilePath', ''),
-                                     gmtFilePath=request.get('gmtFilePath', ''),
-                                     outFilePath=outFilePath,
-                                     numRandomIterations=10)
-
-    email_report(request.get('email'), outFilePath)
+    try:
+        gsoa = importr('GSOA')
+        conn = Redis(host="redis")
+        tiger = tasktiger.TaskTiger(connection=conn)
+        args = request.copy()
+        for field in NECESSARY_FIELDS:
+            args.pop(field)
+        if len(str(request.get('dataFilePath'))) < 2:
+            return "no data"
+        outFilePath = "/data/{}.txt".format(request.get('email', 'results_txt').replace('.com', ''))
+        print("email: {}".format(request.get('email', 'results_txt')))       
+        result =  gsoa.GSOA_ProcessFiles(dataFilePath=request.get('dataFilePath', ''),
+                                         classFilePath=request.get('classFilePath', ''),
+                                         gmtFilePath=request.get('gmtFilePath', ''),
+                                         outFilePath=outFilePath,
+                                         numRandomIterations=10)
+        rmarkdown.render('GSOA_Report.Rmd', output_file = outFilePath.replace('txt', 'html'), params = {'dataFile': outFilePath})
+        email_report(request.get('email'), outFilePath)
+    except Exception as e:
+        email_error(request.get('email'), e)
 
 
 # send the email
@@ -46,20 +50,20 @@ def email_report(email_address, file_path):
     msgRoot['Subject'] = 'GSOA Results'
     msgRoot['From'] = from_
     msgRoot['To'] = email_address
-
-
-    
     msg = MIMEMultipart('alternative')
     text = MIMEText('Results File', 'plain')
     msgRoot.attach(msg)
-
-
     with open(file_path) as fp:
         attachment = MIMEBase('text', None)
         attachment.set_payload(fp.read())
         attachment.add_header('Content-Disposition', 'attachment', filename=file_path.split('/')[-1])
     msgRoot.attach(attachment)
     
+    with open(file_path.replace('txt', 'html')) as fp:
+        attachment1 = MIMEBase('html', None)
+        attachment1.set_payload(fp.read())
+        attachment1.add_header('Content-Disposition', 'attachment', filename=file_path.split('/')[-1])
+    msgRoot.attach(attachment1)
     #msg.attach(text)
     mailer = smtplib.SMTP('smtp.gmail.com:587')
     mailer.starttls()
@@ -68,5 +72,20 @@ def email_report(email_address, file_path):
     mailer.close()
 
 
+def email_error(email_address, exception):
+    from_ = 'smacneil88@gmail.com'
+    msgRoot = MIMEMultipart('related')
+    msgRoot['Subject'] = 'GSOA Results'
+    msgRoot['From'] = from_
+    msgRoot['To'] = email_address
+    msg = MIMEMultipart('alternative')
+    text = MIMEText('There was an error running GSOA. {}'.format(exception), 'plain')
+    msgRoot.attach(msg)
 
+    #msg.attach(text)
+    mailer = smtplib.SMTP('smtp.gmail.com:587')
+    mailer.starttls()
+    mailer.login('smacneil88', 'Berkeley13')
+    mailer.sendmail(from_, email_address, msgRoot.as_string())
+    mailer.close()
 
