54de98a3bf2ba002b35f29559b600200293661f7
smacneil1 <smacneil88@gmail.com>
2017-03-23 19:19:30 -0600

    chagned gsoa email

--- gsoa_task.py
+++ gsoa_task.py
@@ -1,4 +1,4 @@
-import time
+
 import tasktiger
 from redis import Redis 
 from rpy2.robjects.packages import importr
@@ -9,9 +9,7 @@ from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 import smtplib
 from rpy2.robjects import ListVector
-import sys 
-import os
-import traceback
+import sys # DELETE
 
 gsoa = importr('GSOA')
 rmarkdown = importr('rmarkdown')
@@ -28,31 +26,6 @@ ACCEPTED_FIELDS = ['outFilePath', 'classificationAlgorithm', 'numCrossValidation
 def call_gsoa(request):
     print("request: {}".format(request))
     print(NECESSARY_FIELDS)
-# <<<<<<< HEAD
-#    # result=''
-#   #  try:
-#     stdout_file = open('stdout.txt', 'w')
-#     sys.stdout = stdout_file
-#     gsoa = importr('GSOA')
-#     conn = Redis(host="redis")
-#     tiger = tasktiger.TaskTiger(connection=conn)
-#     args = request.copy()
-#     for field in NECESSARY_FIELDS:
-#         args.pop(field)
-#     if len(str(request.get('dataFilePath'))) < 2:
-#         return "no data"
-#     outFilePath = "/data/{}-{}.txt".format(os.urandom(10).encode("hex"), request.get('email', 'results_txt').replace('.com', ''))
-#     print("email: {}".format(request.get('email', 'results_txt')))
-#     result =  gsoa.GSOA_ProcessFiles(dataFilePath=request.get('dataFilePath', ''),
-#                                      classFilePath=request.get('classFilePath', ''),
-#                                      gmtFilePath=request.get('gmtFilePath', ''),
-#                                      outFilePath=outFilePath,
-#                                      numRandomIterations=request.get('numRandomIterations', ''),
-#                                      classificationAlgorithm=request.get('classificationAlgorithm', ''), 
-#                                      numCrossValidationFolds=request.get('numCrossValidationFolds', ''), 
-#                                      removePercentLowestExpr=request.get('removePercentLowestExpr', ''), 
-#                                      removePercentLowestVar=request.get('removePercentLowestVar', ''))
-# =======
     local_buffer = []
     try:
         gsoa = importr('GSOA')
@@ -86,71 +59,6 @@ def call_gsoa(request):
         rinterface.set_writeconsole_regular(rinterface.consolePrint)
 
 
-    #print(result)
-    
-
-    print("Writing RMarkdown")
-    
-    outFilePath_html=outFilePath.replace('txt', 'html')
-    rmarkdown.render('/app/GSOA_Report.Rmd', output_file = outFilePath.replace('txt', 'html'),
-        params=ListVector({'data1': outFilePath}))
-    email_report(request.get('email'), outFilePath)
-    stdout_file.close()
-
-
-        
-    #except Exception as e:
-        #stdout= standoutfile
-        #stdout_file = open('file1.txt', 'w')
-        #sys.stdout = stdout_file
-        #print(result)
-        #trace = traceback.format_exc()
-        #email_error(request.get('email'), e, trace)
-        #stdout_file.close()
-    #    stdout_file = open('file1.txt', 'w')
-    #    sys.stdout = stdout_file
-    #    print(result)
-        #stdout_file.close()
- 
-    #finally:   
-    
- 
-#@tiger.task()
-#def call_gsoa_hallmarks(request):
-#    print("request: {}".format(request))
-#    print(NECESSARY_FIELDS)
-#    try:
-#        gsoa = importr('GSOA')
-#        conn = Redis(host="redis")
-#        tiger = tasktiger.TaskTiger(connection=conn)
-#        args = request.copy()
-##       for field in NECESSARY_FIELDS:
-#            args.pop(field)
-#       if len(str(request.get('dataFilePath'))) < 2:
-#            return "no data"
- #       outFilePath = "/data/{}.txt".format(request.get('email', 'results_txt').replace('.com', ''))
- #       print("email: {}".format(request.get('email', 'results_txt')))       
- #       result =  gsoa.GSOA_ProcessFiles(dataFilePath=request.get('dataFilePath', ''),
- #                                        classFilePath=request.get('classFilePath', ''),
- #                                        gmtFilePath=request.get('gmtFilePath', ''),
- #                                        outFilePath=outFilePath,
- #                                        numRandomIterations=request.get('numRandomIterations', ''),
- #                                        classificationAlgorithm=request.get('classificationAlgorithm', ''), 
- #                                        numCrossValidationFolds=request.get('numCrossValidationFolds', ''), 
- #                                        removePercentLowestExpr=request.get('removePercentLowestExpr', ''), 
- #                                        removePercentLowestVar=request.get('removePercentLowestVar', ''))     
- #        print("Writing RMarkdown")
- #       outFilePath_html=outFilePath.replace('txt', 'html')
- #       rmarkdown.render('/app/GSOA_Report.Rmd', output_file = outFilePath.replace('txt', 'html'),
- #           params=ListVector({'data1': outFilePath}))
- #       email_report(request.get('email'), outFilePath)
- #   except Exception as e:
- #       email_error(request.get('email'), e)
-
-
-
-
-
 def email_report(email_address, file_path):
     from_ = 'gsoa.app@gmail.com'
     msgRoot = MIMEMultipart('related')
@@ -178,34 +86,15 @@ def email_report(email_address, file_path):
     mailer.sendmail(from_, email_address, msgRoot.as_string())
     mailer.close()
 
-BODY = """GSOA returned the following error:
 
-{}
-
-If you have further questions, please email gsoa.app@gmail.com
-"""
-
-#<<<<<<< HEAD
-#def email_error(email_address, exception,trace):
-#    from_ = 'gsoa.app@gmail.com'
-#=======
 def email_error(email_address, exception, local_buffer):
-    from_ = 'smacneil88@gmail.com'
+    from_ = 'gsoa.app@gmail.com'
     msg = MIMEMultipart()
     msg['From'] = from_
     msg['To'] = email_address
     msg['Subject'] = "GSOA ERROR"
-#<<<<<<< HEAD
-    #body = BODY.format(str(exception).strip())
-#    body = BODY.format(str(trace))
-#=======
     msg.preamble = 'GSOA Returned the Following Error'
-<<<<<<< HEAD
-    body = "Error message: {}: \n {}".format(exception, local_buffer)
-#>>>>>>> 2d036429e0df21578c4a38625837abd466e44324
-=======
     body = "Error message: {}: \n {}".format(exception, '\n'.join(local_buffer))
->>>>>>> f2683822730e609e04d6f0a3bed5456e9cb4a4f3
     msg.attach(MIMEText(body, 'plain'))
     #msg.attach(text)
     mailer = smtplib.SMTP('smtp.gmail.com:587')
@@ -213,4 +102,11 @@ def email_error(email_address, exception, local_buffer):
     mailer.login('gsoa.app', 'p@thway@nalysi$')
     mailer.sendmail(from_, email_address, msg.as_string())
     mailer.close()
+
+
+
+
+
+
+
 
